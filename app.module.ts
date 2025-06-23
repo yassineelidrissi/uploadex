3742460ab6f1fea5192b9e86426c2f944d03a899
yassineelidrissi <yassineelidrissi02@gmail.com>
2025-06-23 21:09:02 +0100

    chore: 🧹 remove .env-based config and simplify AppModule

--- app.module.ts
+++ app.module.ts
@@ -1,17 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UploadsModule } from './lib/uploads.module';
-import { ConfigModule } from '@nestjs/config';
-import uploadConfig from './lib/config/upload.config';
-import environmentsValidation from './lib/config/environments.validation';
-
-const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [UploadsModule, ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: !ENV ? '.env' : `.env.${ENV}`,
-    load: [uploadConfig],
-    validationSchema: environmentsValidation,
-  })],
+  imports: [UploadsModule],
 })
 export class AppModule {}
--- lib/config/environments.validation.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from 'joi';
-
-export default Joi.object({
-    NODE_ENV: Joi.string()
-    .valid('development', 'test', 'production', 'staging')
-    .default('development'),
-    MAX_FILE_SIZE: Joi.number().required(),
-    MAX_FILES: Joi.string().required()
-});
